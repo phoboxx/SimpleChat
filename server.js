f21c8b4305527c31919f5cba5f3ae767974373f1
Frederic Legault <legault.f123@gmail.com>
2020-09-02 01:06:46 -0400

    End of Brad's tutorial

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,24 +21,44 @@ const botName = 'SimpleChat Bot';
 
 // Run when client connects
 io.on('connection', (socket) => {
-  console.log('New WS Connection...');
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  socket.emit('message', formatMessage(botName, 'Welcome to SimpleChat'));
+    socket.join(user.room);
 
-  // Broadcast when a user connects
-  socket.broadcast.emit(
-    'message',
-    formatMessage(botName, 'A user has joined the chat')
-  );
+    // Welcome message
+    socket.emit('message', formatMessage(botName, 'Welcome to SimpleChat'));
 
-  // Runs when client disconnects
-  socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName, 'A user as left the chat'));
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users to room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
   // Listen for chatMessage
   socket.on('chatMessage', (msg) => {
-    io.emit('message', msg);
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} as left the chat`)
+      );
+    }
   });
 });
 
